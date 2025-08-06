16e4c16baaa542ea8bef86047cae2958bfef8cc0
Sujit ghosh <sujitghosh07.official@gmail.com>
2025-08-06 21:58:18 +0530

    working on logic

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
-const weather = document.getElementById("container");
-const number = document.getElementById("weather")
+const weatherForm = document.querySelector(".weatherform");
+const cityInput = document.querySelector(".cityInput");
+const card = document . querySelector("card-content");
+const cityDisplay = document.querySelector(".cityDisplay");
+const descDisplay = document.querySelector(".descDisplay");
+const weatherEmoji = document.querySelector(".weatherEmoji");
+const apiKey = c334699d67487efa86e141b4f7a29e97;
+
+weatherForm.addEventListener("sumit" , event =>{
+    event.preventDefault();
+
+    const city = cityInput.value;
+    if(city){
+
+    }
+    else{
+        displayError("please enter a city");
+    }
+});
+
+async function getWeatherData(city){
+
+}
+
+function displayWeatherInfo(data){
+
+}
+
+function getweatherEmoji(weatherid){
+
+}
+function displayError(message){
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent= message;
+    errorDisplay.classList.add("errorDisplay");
+
+    card.textContent ="";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
+}
